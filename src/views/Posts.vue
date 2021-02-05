35f7a11028c5df2be27cc940353586a5eaf2c059
IvanGH4 <ivangarciaharriague07@gmail.com>
2021-02-04 21:41:08 -0300

    fix style posts

--- src/views/Posts.vue
+++ src/views/Posts.vue
@@ -5,14 +5,14 @@
       <input type="text" @change="filteredPosts" class="shadow-inner rounded-xl px-4 py-2  mb-2" placeholder="Search by category" v-model="searchTerm" />
 
       <div class="p-4 w-full md:w-1/2 h-auto border border-gray-400 rounded-sm shadow-md mb-4 relative" v-for="(post, idx) in filteredPosts" :key="idx">
-        <h2 class="font-bold text-4xl text-center mb-4">{{post.title}}</h2>
+        <h2 class="font-bold text-3xl text-center mb-4">{{post.title}}</h2>
         <div class="flex justify-center">
           <div class="spinner w-12 h-12 rounded-full border-2 border-dashed border-green-500" v-if="loading"></div>
-          <img class="w-full md:w-1/4 rounded-md" :src="post.imageUrl" alt="#" v-if="!loading" />
+          <img class="w-full md:w-2/4 rounded-md" :src="post.imageUrl" alt="#" v-if="!loading" />
         </div>
         <span class="absolute top-0 right-0 px-4 bg-myBlue-light text-white font-bold tracking-wider rounded-bl-xl">{{post.category}}</span>
-        <p class="text-gray-600 mb-2">{{post.body}}</p>
-        <a :href="post.link" target="_blank" class="text-sm text-myBlue-light">More info</a>
+        <p class="text-gray-600 my-2">{{post.body}}</p>
+        <a :href="post.link" target="_blank" class="text-sm text-myBlue-light">Visit site</a>
         <div class="flex justify-center items-end flex-col">
           <div class="flex items-end">
             <img class="w-10 h-10 rounded-full mr-1" :src="post.photoUrl" alt="#" />
